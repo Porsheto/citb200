742944a17d1a1133c412cd8704086ef463927554
Totko Duhlakov <totko.petkov.duhlakov@gmail.com>
2019-04-07 11:35:34 +0100

    changes 6

--- lab1/src/textprinter.cpp
+++ lab1/src/textprinter.cpp
@@ -1,7 +1,11 @@
 #include "textprinter.h"
 void TextPrinter::print(std::ostream &out, Invoice invoice){
     for ( auto item : invoice.getItems()) {
-        out << item.getQuantity()<< " x " << item.getProduct().getName() << " (" << item.getProduct().getPrice()  << ")" << std::endl;
+    out << item.getQuantity() << " x "
+        << item.getProduct().getName() 
+        << " (" << item.getProduct().getPrice() << ")"
+        << " = "  << invoice.computeItemTotal(item)
+        << std::endl;
 
     }
    
